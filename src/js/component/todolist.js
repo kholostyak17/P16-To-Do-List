19e2f6578e2fd527a110a15d5866d9a1ff125004
kholostyak17 <kholostyak17@gmail.com>
2021-05-12 08:38:52 +0000

    added map but still not working

--- src/js/component/todolist.js
+++ src/js/component/todolist.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Task from "./task.js";
 
 const ToDoList = () => {
@@ -8,22 +8,28 @@ const ToDoList = () => {
 		return <div className="bg-white text-dark">{newTask}</div>;
 	}*/
 	const [list, setList] = useState([]);
-	//let listMap = list.map(() => {});
+	let listMap = "";
+
+	useEffect(() => {
+		listMap = list.map((task, index) => {
+			return <li key={index.toString()}>{task}</li>;
+		});
+		console.log(listMap);
+	}, [list]);
 
 	return (
-		<div className="bg-danger text-white p-4 col">
+		<div className="bg-danger text-dark p-4 col">
 			<p>To Do List</p>
 			<input
 				type="text"
 				placeholder="add a new task here and push enter"
 				onKeyPress={event => {
 					if (event.key == "Enter") {
-						setList(...list, event.target.value);
-						console.log(list);
+						setList([...list, event.target.value]);
 					}
 				}}></input>
 			<section>
-				<ul></ul>
+				<ul>{listMap}</ul>
 			</section>
 		</div>
 	);
